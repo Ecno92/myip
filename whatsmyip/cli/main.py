8f3c613a45ae7f79b1f56c9dc6013e5cc1e83364
Therry van Neerven <mail@therryvanneerven.nl>
2018-08-08 12:43:06 +0200

    removed click group

--- whatsmyip/cli/main.py
+++ whatsmyip/cli/main.py
@@ -4,11 +4,7 @@ from whatsmyip.ip import get_ip
 from whatsmyip.providers import GoogleDnsProvider, ip_providers
 
 
-@click.group()
-def cli():
-    pass
-
-@cli.command(help='Query for the external IP address')
+@click.command(help='Query for the external IP address')
 @click.option('--provider', type=click.Choice(ip_providers.keys()),
               default=GoogleDnsProvider.name,
               help='name of the provider')
