c4ab08fdb311cca49177d4930e98029b46deacf3
Therry van Neerven <mail@therryvanneerven.nl>
2018-09-10 20:09:56 +0200

    Updates to packaging

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
+import os
+import sys
+
 from setuptools import setup
+
+CURDIR = os.path.realpath(os.path.join(os.path.dirname(
+    sys.modules['__main__'].__file__)))
+sys.path.insert(0, os.path.join(CURDIR, 'src'))
 
 if __name__ == '__main__':
     setup()
