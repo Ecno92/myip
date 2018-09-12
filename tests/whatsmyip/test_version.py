ee73acda6b2e03b91a60c8866d1fe08d683c3d17
Therry van Neerven <mail@therryvanneerven.nl>
2018-09-12 15:26:38 +0200

    ensure not post or pre release

--- tests/whatsmyip/test_version.py
+++ tests/whatsmyip/test_version.py
@@ -6,3 +6,5 @@ from whatsmyip.__version__ import __version__ as app_version
 def test_version():
     v = pkg_resources.parse_version(app_version)
     assert isinstance(v, pkg_resources.extern.packaging.version.Version)
+    assert v.is_prerelease is False
+    assert v.is_postrelease is False
