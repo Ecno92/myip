8a9be5fcab48c47c27cd60806d76b16adff2acf3
Therry van Neerven <mail@therryvanneerven.nl>
2018-09-12 15:13:51 +0200

    replaced version check by distutils by pkg_resources (setuptools)

--- tests/whatsmyip/test_version.py
+++ tests/whatsmyip/test_version.py
@@ -1,6 +1,8 @@
-from distutils.version import StrictVersion
+import pkg_resources
+
 from whatsmyip.__version__ import __version__ as app_version
 
 
 def test_version():
-    StrictVersion(app_version)
+    v = pkg_resources.parse_version(app_version)
+    assert isinstance(v, pkg_resources.extern.packaging.version.Version)
